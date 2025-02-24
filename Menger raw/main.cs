1004f8b87a00d956c81dc303c833cf1b6517e291
Matthew-Li-mtl <matthewli.mtl@gmail.com>
2025-02-23 16:13:32 -0800

    Add files via upload

---
+++ Menger raw/main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+    public static void Main (string[] args) {
+        Menger mydata = new Menger("mydata");
+        mydata.addItem("root", "new item");
+        mydata.addItem("root/new item", "another item");
+        
+        
+        mydata.printAll("root");
+        Console.WriteLine(mydata.data);
+    }
+}
